4aef28e115e937483c756cbb1e0b7bb80ed29768
Student <example@example.com>
2019-11-27 12:26:58 -0800

    setup webpack

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// UTILITIES
+import $ from 'jquery';
+import 'bootstrap';
+import './css/bootstrap.min.css';
+import './css/styles.css';
+
+// MAIN LOGIC
+import { User } from './business.js';
+
+
+
+// USER INTERFACE
+$(document).ready(function(){
+
+  
+});
